2b2042484af1a63589bde03f5f5dcc63f4b23165
norkitorki <norki@tuta.io>
2022-10-14 07:17:29 +0200

    Use more specific replace_all attributes

--- app/javascript/controllers/input_controller.js
+++ app/javascript/controllers/input_controller.js
@@ -14,7 +14,9 @@ export default class extends Controller {
     let lastField = this.fieldTargets[newIndex - 1];
     let newField = lastField.cloneNode(true);
 
-    newField.innerHTML = newField.innerHTML.replaceAll(newIndex - 1, newIndex);
+    newField.innerHTML = newField.innerHTML
+      .replaceAll(`_attributes_${newIndex - 1}_`, `_attributes_${newIndex}_`)
+      .replaceAll(`_attributes][${newIndex - 1}]`, `_attributes][${newIndex}]`);
     // newField.querySelector('input').value = "";
     lastField.after(newField);
   }
