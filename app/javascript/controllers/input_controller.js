4d563692d07e489ada1d284d35966b6b66cb68aa
norkitorki <norki@tuta.io>
2022-10-15 08:22:41 +0200

    Add remove method and refactor controller to be more reusable

--- app/javascript/controllers/input_controller.js
+++ app/javascript/controllers/input_controller.js
@@ -2,22 +2,39 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="input"
 export default class extends Controller {
-  static targets = [ "field", "addLink" ]
+  static classes = [ "field" ]
 
   connect() {
-    this.addLinkTarget.hidden = false;
+    this.element.hidden = false;
+  }
+
+  fields() {
+    if (this.hasFieldClass) {
+      return document.querySelectorAll(`.${this.fieldClass}`)
+    } else {
+      console.log("Please provide a data-input-field-class to the controller element.")
+    }
   }
 
   add(event) {
     event.preventDefault();
-    let newIndex = this.fieldTargets.length;
-    let lastField = this.fieldTargets[newIndex - 1];
+
+    let fields = this.fields();
+    let newIndex = fields.length;
+    let lastField = fields[newIndex - 1];
     let newField = lastField.cloneNode(true);
 
     newField.innerHTML = newField.innerHTML
       .replaceAll(`_attributes_${newIndex - 1}_`, `_attributes_${newIndex}_`)
       .replaceAll(`_attributes][${newIndex - 1}]`, `_attributes][${newIndex}]`);
-    // newField.querySelector('input').value = "";
+    newField.querySelector('input').value = "";
     lastField.after(newField);
+  }
+
+  remove(event) {
+    event.preventDefault();
+
+    let fields = this.fields();
+    fields[fields.length - 1].remove();
   }
 }
